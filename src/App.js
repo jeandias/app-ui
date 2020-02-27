df6211f5b0861a1c0fbc087b45597acf170fb3ce
Jean Dias <jeanpeixotodias@gmail.com>
2020-02-27 18:41:35 +0000

    Create service

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
+const URL = 'http://localhost:3000/api/v1';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  validatePassword(value) {
+    axios.post(`${URL}/users/validate_password`, { user: { password: value } }).then(resp => console.log());
+  }
+
+  handleChange(event) {
+    this.setState(
+      { value: event.target.value },
+      () => {
+        if (this.state.value && this.state.value.length > 1) {
+          this.validatePassword(this.state.value)
+        }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form>
+            <input type="password" value={this.state.value} onChange={this.handleChange} />
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
